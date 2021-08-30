eb5ba5cf7d2cf5b065983cc73c1c536222aae2a2
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-08-31 00:09:21 +0530

    Added 3 dummy components into client.

--- react-nest-video-app/client/src/App.tsx
+++ react-nest-video-app/client/src/App.tsx
@@ -1,8 +1,55 @@
+import { makeStyles, Typography, AppBar } from '@material-ui/core';
 import React from 'react';
 import './App.css';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
+import VideoPlayer from './components/VideoPlayer';
+
+const useStyles = makeStyles((theme) => ({
+	appBar:
+		{
+			borderRadius: 15,
+			margin: '30px 100px',
+			display: 'flex',
+			flexDirection: 'row',
+			justifyContent: 'center',
+			alignItems: 'center',
+			width: '600px',
+			border: '2px solid black',
+
+			[theme.breakpoints.down('xs')]:
+				{
+					width: '90%'
+				}
+		},
+	image:
+		{
+			marginLeft: '15px'
+		},
+	wrapper:
+		{
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+			width: '100%'
+		}
+}));
 
 function App() {
-	return <div>Video Chat App</div>;
+	const classes = useStyles();
+	return (
+		<div className={classes.wrapper}>
+			<AppBar className={classes.appBar} position="static" color="inherit">
+				<Typography variant="h2" align="center">
+					Video Chat
+				</Typography>
+			</AppBar>
+			<VideoPlayer />
+			<Options>
+				<Notifications />
+			</Options>
+		</div>
+	);
 }
 
 export default App;
